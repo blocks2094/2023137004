9401278494d79aca821dba5c1fa50ce04aa5c6d2
blocks2094 <blocks2094@g.shingu.ac.kr>
2023-04-26 00:09:23 +0900

    1

---
+++ 02.Scrips/PlayerBall.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+    public float jumpPower;
+    bool isJump;
+    Rigidbody rigid;
+
+    private void Awake()
+    {
+        isJump = false;
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }     
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "floor")
+        {
+            isJump = false;
+        }
+    }
+}
